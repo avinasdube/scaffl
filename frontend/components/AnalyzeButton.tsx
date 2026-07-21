20e190d4dc789aa6ec8c830f34d6eebfe1e22553
avinasdube <avidubey712@gmail.com>
2026-07-21 17:14:18 +0530

    feat: update AnalyzeButton to show loading state during analysis

--- frontend/components/AnalyzeButton.tsx
+++ frontend/components/AnalyzeButton.tsx
@@ -1,15 +1,17 @@
 type AnalyzeButtonProps = {
     onClick: () => void;
+    loading: boolean
 };
 
 export default function AnalyzeButton({
     onClick,
+    loading
 }: AnalyzeButtonProps){
     return (
         <button
         onClick={onClick}
         className="w-fit h-fit pl-4 pr-4 pt-2 pb-2 rounded-lg bg-blue-500 text-white">
-            Analyze
+            {loading ? "Analyzing" : "Analyze"}
         </button>
     )
 }
